eab97eb94a74335de7f912973b415ff3590f58bb
Junaid Rasheed <junaidrasheed1@gmail.com>
2022-02-14 00:52:38 +0500

    gh-pages

---
+++ js/chunk-2d0aa1f6.184180f6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aa1f6"],{1056:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},r=[],a={name:"FirstModuleIndex"},l=a,c=t("2877"),i=Object(c["a"])(l,u,r,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0aa1f6.184180f6.js.map
---
+++ js/chunk-2d0b9a05.714c4968.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b9a05"],{"346d":function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},u=[],a={name:"TradesIndex"},c=a,l=t("2877"),s=Object(l["a"])(c,r,u,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0b9a05.714c4968.js.map
